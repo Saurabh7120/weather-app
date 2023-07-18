eaba6dc02eba513872eb242da25c0d108dca5f68
Saurabh Mane <saurabhmane1997@gmail.com>
2023-07-17 21:19:56 -0400

    url change

--- frontend/src/api/getWeatherByCity.js
+++ frontend/src/api/getWeatherByCity.js
@@ -4,9 +4,9 @@ export const getWeatherByCity = async (data) => {
     const {city, lat,long} = data
     let res;
     if(city) {
-         res = await axios(`https://weather-app-service-h8yu.onrender.com/getWeatherByCity?city=${city}}`)
+         res = await axios(`https://staging--unique-dolphin-1c8e27.netlify.app/.netlify/functions/api/getWeatherByCity?city=${city}}`)
     }else{
-        res = await axios(`https://weather-app-service-h8yu.onrender.com/getWeatherByCity?lat=${lat}&long=${long}`)
+        res = await axios(`https://staging--unique-dolphin-1c8e27.netlify.app/.netlify/functions/api/getWeatherByCity?lat=${lat}&long=${long}`)
     }
     return res.data
 }
